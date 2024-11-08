42562030af4339074da02223c4c326a927eba1f0
colorfulparadox <101310062+colorfulparadox@users.noreply.github.com>
2024-11-08 10:23:51 -0700

    work on tablecontroller

--- Game/Assets/Scripts/TableController.cs
+++ Game/Assets/Scripts/TableController.cs
@@ -3,17 +3,32 @@ using UnityEngine;
 public class TableController : MonoBehaviour
 {
 
-    public GameObject Customer;
+    public GameObject CustomerObj;
+    CustomerController customer;
+
+    Transform tableTransform;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        tableTransform = this.GetComponent<Transform>();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+
+    public void AddCustomer(GameObject customer)
+    {
+        this.CustomerObj = customer;
+        CustomerController cust = customer.GetComponent<CustomerController>();
+        if (cust != null)
+        {
+            throw new System.Exception("Customer object does not have a customer controller!");
+        }
+        this.customer = cust;
     }
 }

a5854b6eb9665767c43262668ea90b4384b2a418
Preston <101310062+colorfulparadox@users.noreply.github.com>
2025-02-01 15:42:47 -0700

    merge scripts 2

---
+++ game/assets/orderMenuController.gd
@@ -0,0 +1,13 @@
+extends Node
+
+var itemElement: PackedScene = load('res://nodes/item_element.tscn')
+
+# {"name": "value", "price": 0, "time": 0}
+
+func _ready():
+	pass
+	
+
+
+func _process(delta):
+	pass

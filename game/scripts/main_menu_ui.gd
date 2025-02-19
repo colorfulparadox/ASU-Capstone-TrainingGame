a591819596a64459b4e3c217045f74c65aa5d921
Skyler Riske <sriske2@users.noreply.github.com>
2025-02-19 16:59:20 -0700

    start button goes to restaurant.
    Added return to menu button to restaurant scene

--- game/scripts/main_menu_ui.gd
+++ game/scripts/main_menu_ui.gd
@@ -12,7 +12,7 @@ func _process(delta: float) -> void:
 
 
 func _on_start_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://game.tscn")
+	get_tree().change_scene_to_file("res://nodes/restaurant_scene.tscn")
 
 
 func _on_options_button_pressed() -> void:

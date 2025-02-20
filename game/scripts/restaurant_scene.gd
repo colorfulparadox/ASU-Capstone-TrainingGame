5d2a58d68d18623c067aaa1edb83277c551f0f94
Skyler Riske <sriske2@users.noreply.github.com>
2025-02-19 19:55:10 -0700

    add test button to spawn an order page on restaurant scene

--- game/scripts/restaurant_scene.gd
+++ game/scripts/restaurant_scene.gd
@@ -12,3 +12,12 @@ func _process(delta: float) -> void:
 
 func _on_return_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://nodes/main_menu_ui.tscn")
+
+func _on_show_order_page() -> void:
+	
+#	This will spawn an order scene that's shown at the top zindex
+	var orderpagescene: PackedScene = load("res://nodes/order_page.tscn")
+	var instance = orderpagescene.instantiate()
+	instance.position = get_viewport().size * -0.5
+	instance.set_z_index(100)
+	add_child(instance)

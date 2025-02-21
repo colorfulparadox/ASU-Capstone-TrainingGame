9197e7729fefb4cb5aa2adbced55cbd178fe8148
BluSnake57 <minewhip@gmail.com>
2025-02-21 10:46:17 -0700

    api node and basic api functions

---
+++ game/nodes/login.gd
@@ -0,0 +1,22 @@
+extends Button
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+	
+	
+
+
+func _on_pressed() -> void:
+	var username_input = get_tree().get_nodes_in_group("username")[0]
+	var password_input = get_tree().get_nodes_in_group("password")[0]
+	var api_node = get_tree().get_nodes_in_group("api_node")[0]
+	
+	# api_node.ping()
+	api_node.sign_in(username_input.get_line(0), password_input.get_line(0))
--- game/scripts/api_handler.gd
+++ game/scripts/api_handler.gd
@@ -1,44 +1,66 @@
 extends Node
 
-const url := "https://backend.project_persona.com"
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+const url := "https://backend.project-persona.com"
 	
 func start_conversation():
-	var err = 0
-	var http = HTTPClient.new() # Create the Client.
-
-	err = http.connect_to_host(url, 443) # Connect to host/port.
-	assert(err == OK) # Make sure connection is OK.
-
-	# Wait until resolved and connected.
-	while http.get_status() == HTTPClient.STATUS_CONNECTING or http.get_status() == HTTPClient.STATUS_RESOLVING:
-		http.poll()
-		print("Connecting...")
-		await get_tree().process_frame
-
-	assert(http.get_status() == HTTPClient.STATUS_CONNECTED) # Check if the connection was made successfully.
-
+	$HTTPRequest.request(url)
+	
+func continue_conversation():
+	$HTTPRequest.request(url)
+	
+func end_converstion():
+	$HTTPRequest.request(url)
+	
+func sign_in(username:String, password:String) -> String:
+	
 	# Some headers
-	var headers = [
+	var header = [
 		"User-Agent: Pirulo/1.0 (Godot)",
 		"Accept: */*"
 	]
+	
+	var body = {
+		"username": username,
+		"password": password
+	}
+	
+	var json_body = JSON.stringify(body)
 
-	err = http.request(HTTPClient.METHOD_GET, "/ChangeLog-5.php", headers) # Request a page from the site (this one was chunked..)
+	var response = await post_request("/login", header, json_body)
+	return response
+	
+func final_score():
+	$HTTPRequest.request(url)
+	
+func ping():
+	var header = []
+
+	var response = await get_request("/ping", header)
+	return response
+	
+func get_request(extension:String, header:PackedStringArray) -> String:
+	var err = 0
+	var http = HTTPClient.new() # Create the Client.
+	
+	err = http.connect_to_host(url, 443) # Connect to host/port.
+	assert(err == OK) # Make sure connection is OK.
+	
+	while http.get_status() == HTTPClient.STATUS_CONNECTING or http.get_status() == HTTPClient.STATUS_RESOLVING:
+		http.poll()
+		print("Connecting...")
+		print(http.get_status())
+		await get_tree().process_frame
+
+	assert(http.get_status() == HTTPClient.STATUS_CONNECTED)
+	
+	err = http.request(HTTPClient.METHOD_GET, extension, header) # Request a page from the site (this one was chunked..)
 	assert(err == OK) # Make sure all is OK.
 
 	while http.get_status() == HTTPClient.STATUS_REQUESTING:
 		# Keep polling for as long as the request is being processed.
 		http.poll()
 		print("Requesting...")
+		print(http.get_status())
 		await get_tree().process_frame
 
 	assert(http.get_status() == HTTPClient.STATUS_BODY or http.get_status() == HTTPClient.STATUS_CONNECTED) # Make sure request finished well.
@@ -48,9 +70,9 @@ func start_conversation():
 	if http.has_response():
 		# If there is a response...
 
-		headers = http.get_response_headers_as_dictionary() # Get response headers.
+		var new_header = http.get_response_headers_as_dictionary() # Get response headers.
 		print("code: ", http.get_response_code()) # Show response code.
-		print("**headers:\\n", headers) # Show headers.
+		print("**headers:\\n", new_header) # Show headers.
 
 		# Getting the HTTP Body
 
@@ -80,21 +102,76 @@ func start_conversation():
 		print("bytes got: ", rb.size())
 		var text = rb.get_string_from_ascii()
 		print("Text: ", text)
+		return text
+	return "Error"
+	http.quit()
 	
+func post_request(extension:String, header:PackedStringArray, body:String) -> String:
+	var err = 0
+	var http = HTTPClient.new() # Create the Client.
+
+	err = http.connect_to_host(url, 443) # Connect to host/port.
+	assert(err == OK) # Make sure connection is OK.
+
+	# Wait until resolved and connected.
+	while http.get_status() == HTTPClient.STATUS_CONNECTING or http.get_status() == HTTPClient.STATUS_RESOLVING:
+		http.poll()
+		print("Connecting...")
+		print(http.get_status())
+		await get_tree().process_frame
+
+	assert(http.get_status() == HTTPClient.STATUS_CONNECTED) # Check if the connection was made successfully.
 	
-func continue_conversation():
-	$HTTPRequest.request(url)
-	
-	
-func end_converstion():
-	$HTTPRequest.request(url)
-	
-	
-func sign_in():
-	$HTTPRequest.request(url)
-	
-	
-func final_score():
-	$HTTPRequest.request(url)
-	
+	err = http.request(HTTPClient.METHOD_POST, extension, header, body) # Request a page from the site (this one was chunked..)
+	assert(err == OK) # Make sure all is OK.
+
+	while http.get_status() == HTTPClient.STATUS_REQUESTING:
+		# Keep polling for as long as the request is being processed.
+		http.poll()
+		print("Requesting...")
+		print(http.get_status())
+		await get_tree().process_frame
+
+	assert(http.get_status() == HTTPClient.STATUS_BODY or http.get_status() == HTTPClient.STATUS_CONNECTED) # Make sure request finished well.
+
+	print("response? ", http.has_response()) # Site might not have a response.
+
+	if http.has_response():
+		# If there is a response...
+
+		var new_header = http.get_response_headers_as_dictionary() # Get response headers.
+		print("code: ", http.get_response_code()) # Show response code.
+		print("**headers:\\n", new_header) # Show headers.
+
+		# Getting the HTTP Body
+
+		if http.is_response_chunked():
+			# Does it use chunks?
+			print("Response is Chunked!")
+		else:
+			# Or just plain Content-Length
+			var bl = http.get_response_body_length()
+			print("Response Length: ", bl)
+
+		# This method works for both anyway
+
+		var rb = PackedByteArray() # Array that will hold the data.
+
+		while http.get_status() == HTTPClient.STATUS_BODY:
+			# While there is body left to be read
+			http.poll()
+			# Get a chunk.
+			var chunk = http.read_response_body_chunk()
+			if chunk.size() == 0:
+				await get_tree().process_frame
+			else:
+				rb = rb + chunk # Append to read buffer.
+		# Done!
+		print("bytes got: ", rb.size())
+		var text = rb.get_string_from_ascii()
+		print("Text: ", text)
+		
+		return text
+	return "Error"
+	http.quit()
 	

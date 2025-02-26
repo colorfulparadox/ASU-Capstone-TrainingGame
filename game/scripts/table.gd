f4c28358a18e3ba8143340225479fc3109c55d95
ptorpeyASU <ptorpey@asu.edu>
2025-02-26 13:00:51 -0700

    Tables are clickable and brings player to order_page after customer sits down

--- game/scripts/table.gd
+++ game/scripts/table.gd
@@ -2,10 +2,15 @@ extends Area2D
 
 const CUSTOMER = preload("res://nodes/customer.tscn")
 
+@onready var button = $tableButton
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	add_to_group("unoccupied_table")
-	pass # Replace with function body.
+	button.visible = false
+	button.disabled = true
+	pass
+
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -27,6 +32,9 @@ func _on_body_entered(body: Node2D) -> void:
 	if body.is_in_group("customer") && is_in_group("unoccupied_table"):
 		table_occupied()
 		body.queue_free()
+		button.visible = true
+		button.modulate.a = 0
+		button.disabled = false
 	pass # Replace with function body.
 
 func table_occupied():
@@ -46,3 +54,25 @@ func table_unoccupied():
 func _on_timer_timeout() -> void:
 	customer_exit()
 	pass # Replace with function body.
+
+
+func _on_table_button_pressed() -> void:
+	print("Button was clicked!")
+	$Timer.paused = true
+	var instance = spawn_order_page(4)
+	add_child(instance)
+	pass # Replace with function body.
+
+func spawn_order_page(test_value: int) -> Node2D:
+	# load packed scene
+	# set properties I guess
+	var orderpagescene: PackedScene = load("res://nodes/order_page.tscn")
+	var instance = orderpagescene.instantiate()
+	
+	# center it
+	instance.position = get_viewport().size * -0.5
+	instance.set_z_index(100)
+	
+	instance.test_property = test_value
+	
+	return instance

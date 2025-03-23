eaf48df6424882e806c12e65cae120c50572b9fe
Preston <101310062+colorfulparadox@users.noreply.github.com>
2025-03-23 13:14:11 -0700

    char anim script + anim rename

--- game/scripts/char_anim_manager.gd
+++ game/scripts/char_anim_manager.gd
@@ -1,1 +1,15 @@
 extends Node
+
+
+@onready
+var bodyAnim = $BodyAnim
+@onready
+var hairAnim = $HairAnim
+@onready
+var clothingAnim = $ClothingAnim
+
+
+func setAnim(type):
+	bodyAnim.play(type)
+	hairAnim.play(type)
+	clothingAnim.play(type)
